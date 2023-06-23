9d16983088804206351e9b19a7395fe0354ccf22
Jeremy Wells <jmw.jeremymwells@gmail.com>
2023-06-23 11:20:46 -0400

    adding core plus essentials to products page

--- scripts/global.js
+++ scripts/global.js
@@ -183,4 +183,187 @@ $(this).submit(function (e) { // when the form submits
       }
     });
   });
-})
+});
+
+const pricingColumnNodes = document.querySelectorAll('.pricing__bullet-list');
+const originalColumns = [];
+pricingColumnNodes.forEach((n) => originalColumns.push(n.cloneNode(true)));
+
+function setPricingColumnsBody(theSelectedProduct) {
+  const columns = document.querySelectorAll('.pricing__bullet-list');
+  const leftColumn = columns[0];
+  const rightColumn = columns[1];
+  if (theSelectedProduct === 'vue') {
+    const leftBullets = [
+      'Continuous vulnerability scanning',
+      'Modern browser compatibility',
+      '14-day critical patch SLA'
+    ].map((bullet) => {
+      return `
+        <div class="bullet-container">
+          <img src="https://assets.website-files.com/62865614b39c464b76d339aa/63fe08dd56f1ef2552260c0c_check_circle.svg" loading="lazy" width="20" alt="" class="pricing-checkmark">
+          <div class="pricing__bullet-text">${bullet}</div>
+        </div>
+      `
+    })
+    leftColumn.innerHTML = leftBullets.join('')
+    
+    const right_leftBulletItems = [
+      { icon: 'https://herodevs.github.io/webflow/images/nuxt.png', name: 'Nuxt' },
+      { icon: 'https://herodevs.github.io/webflow/images/Vuetify.png', name: 'Vuetify' },
+      { icon: 'https://herodevs.github.io/webflow/images/Quasar.png', name: 'Quasar' },
+    ];
+    const right_rightBulletItems = [
+      { icon: 'https://herodevs.github.io/webflow/images/BootstrapVue.png', name: 'BootstrapVue' },
+      { icon: 'https://herodevs.github.io/webflow/images/vuejs.png', name: 'Vue Router' },
+      { icon: 'https://herodevs.github.io/webflow/images/vuejs.png', name: 'Vuex' },
+    ];
+    const getColumn = (items) => {
+      return `
+        <div class="core-plus-column">
+          ${
+            items.map((item) => `
+              <div class="core-plus-cell">
+                <img src="${item.icon}" loading="lazy" width="20" alt="">
+                <span class="pricing__bullet-text">${item.name}</span>
+              </div>
+            `).join('')
+          }
+        </div>
+      `;
+    }
+
+    rightColumn.innerHTML = `
+      <style>
+
+        .core-plus-parent-container {
+          padding-right: 50px !important;
+        }
+
+        .core-plus-bullet {
+          text-align: center;
+        }
+
+        .core-plus {
+          display: grid;
+          grid-template-columns: repeat(2, 1fr);
+          grid-template-rows: repeat(3, 1fr);
+          grid-auto-flow: column;
+          border-radius: 10px 10px 10px 10px;
+          margin-top: 1em;
+          width: calc(100% + 18px);
+        }
+        
+        .core-plus-column {
+          display: contents;
+        }
+        
+        .core-plus-cell {
+          border: 1px solid #feeefd;
+          text-align: left;
+          padding: 15px 5px 5px 10px;
+          line-height: .5em;
+          white-space: nowrap;
+        }
+
+        .core-plus .core-plus-column:first-child .core-plus-cell:first-child {
+          border-radius: 5px 0 0 0;
+          border-right: 0;
+          border-bottom: 0;
+        }
+        .core-plus .core-plus-column:first-child .core-plus-cell:last-child {
+          border-radius: 0 0 0 5px;
+          border-top: 0;
+          border-right: 0;
+        }
+
+        .core-plus .core-plus-column:first-child .core-plus:nth-of-type(2) {
+          border-right: 0;
+        }
+
+        .core-plus .core-plus-column:last-child .core-plus-cell:last-child {
+          border-radius: 0 0 5px 0;
+          border-top: 0;
+        }
+
+        .core-plus .core-plus-column:last-child .core-plus-cell:first-child {
+          border-radius: 0 5px 0 0;
+          border-bottom: 0;
+        }
+
+        @media screen and (max-width: 959px) {
+          .core-plus-parent-container {
+            padding-right: 0 !important;
+          }
+
+          .core-plus-bullet {
+            margin: 0 10% 0 10%;
+          }
+
+          .core-plus {
+            grid-template-columns: repeat(1, 1fr) !important;
+            grid-template-rows: repeat(6, 1fr) !important;
+            width: 93% !important;
+            margin: auto !important;
+            margin-top: 1em !important;
+          }
+          
+          .core-plus-cell {
+            border: 1px solid #feeefd !important;
+            padding: 15px 5px 5px 9% !important;
+          }
+
+          .core-plus .core-plus-column:first-child .core-plus-cell:first-child {
+            border-radius: 5px 5px 0 0 !important;
+          }
+
+          .core-plus .core-plus-column:first-child .core-plus-cell:last-child {
+            border-radius: 0 0 0 0 !important;
+          }
+
+          .core-plus .core-plus-column:last-child .core-plus-cell:last-child {
+            border-radius: 0 0 5px 5px !important;
+          }
+
+          .core-plus .core-plus-column:last-child .core-plus-cell:first-child {
+            border-radius: 0 0 0 0 !important;
+          }
+        }
+
+      </style>
+
+      <div class="bullet-container" style="padding-right: 0; display: block;">
+
+        <div class="pricing__bullet-text core-plus-bullet" style="white-space: normal">
+          Everything in Core plus compatibility and security patching for:
+        </div>
+
+        
+        <div class="core-plus">
+          ${getColumn(right_leftBulletItems)}
+          ${getColumn(right_rightBulletItems)}
+        </div>
+      </div>
+
+    `;
+    const parent = document.querySelector('.core-plus-bullet').parentElement.parentElement;
+    const parentClasses = parent.getAttribute('class');
+    if (!~parentClasses.indexOf('core-plus-parent-container')) {
+      parent.setAttribute('class', parentClasses + ' ' + 'core-plus-parent-container');
+    }
+  } else {
+    pricingColumnNodes.forEach((column, i) => {
+      column.innerHTML = originalColumns[i].innerHTML;
+    });
+  }
+  //
+}
+if (~window.location.href.indexOf('support/nes-vue')) {
+  const leftPricingColumnHeader = document.getElementById('left-column-header');
+  const rightPricingColumnHeader = document.getElementById('right-column-header');
+  leftPricingColumnHeader.innerText = 'Core';
+  rightPricingColumnHeader.innerText = 'Core + Essentials';
+  setPricingColumnsBody('vue');
+} else {
+  setPricingColumnsBody();
+}
