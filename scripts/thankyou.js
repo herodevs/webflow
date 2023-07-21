fd6efd42ada19a7574f50a43e7e5ca792682c3f7
Diego Julião <dianjuar@gmail.com>
2023-07-21 13:10:36 -0500

    fix: put back the iframe on the script

--- scripts/thankyou.js
+++ scripts/thankyou.js
@@ -1,5 +1,16 @@
 const backButton = document.getElementById('thank-you-go-back');
 
+var html = `
+  <iframe
+    src="https://meetings.hubspot.com/josh-trainque/nes-round-robin"
+    frameborder="0"
+    style="width: 100%; height: 100%; min-height: 730px"
+  />
+`;
+calendarContainer.innerHTML = html;
+calendarDisplayElement.innerHTML = calendarContainer.innerHTML;
+
+
 if (!document.referrer) {
   backButton.style.display = 'none';
 } else {
