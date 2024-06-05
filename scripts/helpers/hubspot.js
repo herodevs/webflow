716c52d9540356e4c14ca8772ce05a62d29cf395
Hiruna Panapura <154359938+hiruna-e46@users.noreply.github.com>
2024-06-06 01:29:15 +0530

    Update hubspot.js
    Vulnerability alert sign up redirect

--- scripts/helpers/hubspot.js
+++ scripts/helpers/hubspot.js
@@ -165,6 +165,8 @@
                 document.location.href = '../form-submitted-newsletter-signup';
               } else if (isOnURL('/vulnerability-directory')) {
                 document.location.href = './vulnerability-alerts-signup';
+              } else if (isOnURL('/vulnerability-directory/')) {
+                document.location.href = '../vulnerability-alerts-signup';
               } else if (isOnURL('support/disclosures')) {
                 /**
                  * page IS disclosures
