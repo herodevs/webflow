e8758ff1e370397f98b2b09f25d1f070e8560d07
Eduardo Roth <eduardo@eduardoroth.dev>
2025-11-05 12:41:53 -0600

    Update iframe source URL in thankyou.jsfix

--- scripts/thankyou.js
+++ scripts/thankyou.js
@@ -6,7 +6,7 @@ const calendarDisplayElement = document.getElementById(
 
 const html = `
   <iframe
-    src="https://meetings.hubspot.com/josh-trainque/nes-round-robin"
+    src="https://meetings.hubspot.com/peter1250/nes-sdr-round-robin"
     frameborder="0"
     style="width: 100%; height: 100%; min-height: 730px"
   />
